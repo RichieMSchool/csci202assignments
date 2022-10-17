c59acd52184f12fad6ae52b9e03b14f240061638
Richie <72110410+RichieMSchool@users.noreply.github.com>
2022-10-17 16:37:51 -0700

    added more to a4

--- js/a4.js
+++ js/a4.js
@@ -2,8 +2,10 @@ size = "250"
 
 $(document).on('mousemove', function(e){
     $('#light').css({"clip-path": `circle(${size}px at ${e.pageX}px ${e.pageY}px)`});
+    $('#circle').css({"left": `${e.pageX - 350}px`, "top": `${e.pageY - 350}px`})
 });
 
 var intervalId = window.setInterval(function(){
     $('#dark-overlay').css({"opacity": Math.random()/5})
   }, 50);
+

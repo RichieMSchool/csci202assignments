8987e99e5d7a6e32ffb5aab45602cfe52f77135f
Richie <72110410+RichieMSchool@users.noreply.github.com>
2022-11-07 14:42:04 -0800

    Completed a7

---
+++ js/a7.js
@@ -0,0 +1,6 @@
+function changeImage(element, file) {
+    // document.getElementById(element).src = window.location.protocol + "//" + window.location.hostname + "/" + file;
+    // console.log(document.getElementById(element).src)
+    // console.log(window.location.protocol + "//" + window.location.hostname + "/" + file);
+    document.getElementById(element).src = file;
+}
